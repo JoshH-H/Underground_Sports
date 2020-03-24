7aba101d2c3ca878f14e8e623825f4d034a76bc5
Josh H-H <jh3771u@greenwich.ac.uk>
2020-03-24 22:30:13 +0000

    TT scoring near completion and intro screen added, needs to be modded tho

---
+++ Menu Prototype/Assets/Scripts/TunaTossing/TTGlobalData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TTGlobalData : MonoBehaviour
+{
+    static public int Scoring;
+    public GameObject player;
+    
+    /*public Text scoreText;
+    public Text scoreText2;*/
+    public GameObject CountDownAnimation;
+    public GameObject Instructions;
+
+    void Awake()
+    {
+        SetDistance();
+        Scoring = 0;
+        Instructions.SetActive(true);
+        CountDownAnimation.SetActive(false);
+        Time.timeScale = 0f;
+
+    }
+    
+    public void SetDistance()
+    {
+        /*scoreText.text = Scoring.ToString();
+        scoreText2.text = Scoring.ToString();*/
+    }
+
+    public void InstructionsComplete()
+    {
+        Instructions.SetActive(false);
+        CountDownAnimation.SetActive(true);
+        StartCoroutine(CountdownCompl());
+        Time.timeScale = 1f;
+    }
+
+    private IEnumerator CountdownCompl()
+    {
+        yield return new WaitForSeconds(4);
+        player.GetComponent<TossTuna>().enabled = true;
+    }
+
+}
---
+++ Menu Prototype/Assets/Scripts/TunaTossing/TunaScoring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TunaScoring : MonoBehaviour
+{
+    public int value = 0;
+    public GameObject distance;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Fish")
+        {
+            distance.SetActive(true);
+            /*GlobalData.Lives--;
+            GameObject.Find("GameBoss").GetComponent<GlobalData>().UpdateLives();*/
+        }
+    }
+    
+}
