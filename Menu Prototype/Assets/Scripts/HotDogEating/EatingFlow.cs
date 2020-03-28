bd3d768e4275b541986b529b3866618b1d0d9a84
Josh H-H <jh3771u@greenwich.ac.uk>
2020-03-28 17:10:01 +0000

    I need you to test this for me Leena

---
+++ Menu Prototype/Assets/Scripts/HotDogEating/EatingFlow.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EatingFlow : MonoBehaviour
+{
+    public Transform foodBar;
+
+    private static float currentFood = 10;
+
+   private static float maxFood = 150;
+
+    //public KeyCode eat;
+    public bool eating_W { get; set; }
+    public float timeCheck = 0;
+    void Start()
+    {
+        foodBar.GetComponent<Image>().color = new Color(0,1,0);
+        eating_W = true;
+    }
+
+    void Update()
+    {
+        if (eating_W)
+        {
+            //FoodBar
+            if (Input.GetKey(KeyCode.W))
+            {
+                timeCheck += Time.deltaTime;
+            }
+            else
+            {
+                if (currentFood < maxFood)
+                {
+                    currentFood += 0.15f;
+                }
+            }
+        }
+
+        if (timeCheck>.50)
+        {
+
+            timeCheck = 0;
+            currentFood -= 10;
+        }
+        
+        if (currentFood>51)
+        {
+            foodBar.GetComponent<Image>().color = new Color(1,1,0);
+        }
+        if (currentFood>100)
+        {
+            foodBar.GetComponent<Image>().color = new Color(1,0,0);
+        }
+        if (currentFood<50)
+        {
+            foodBar.GetComponent<Image>().color = new Color(0,1,0);
+        }
+
+        if (currentFood <= 10)
+        {
+            timeCheck -= Time.deltaTime;
+        }
+        foodBar.GetComponent<RectTransform>().localScale = new Vector3(1, currentFood/maxFood, 1);
+        //FoodBarEND
+    }
+    //Animevents
+    public void disableEat()
+    {
+        eating_W = false;
+        StartCoroutine("restartFood");
+    }
+
+    IEnumerator restartFood()
+    {
+        yield return new WaitForSeconds(2);
+        eating_W = true;
+        Debug.Log("works");
+    }
+}
