db67f7b436db04e0ba74accb76495cb38e6317df
Josh H-H <jh3771u@greenwich.ac.uk>
2020-04-02 19:22:55 +0100

    Final Build PUUUUUSH

---
+++ Menu Prototype/Assets/CloseChamp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CloseChamp : MonoBehaviour
+{
+    //public GameObject Champ;
+
+     public void ChampClose()
+    {
+        //Champ.SetActive(false);        
+        PlayerPrefs.SetInt("Champ", 1);
+        SceneManager.LoadScene("LDScreen");
+    }
+}
