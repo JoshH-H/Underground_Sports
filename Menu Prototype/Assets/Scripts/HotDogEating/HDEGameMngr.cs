61f7fc25089e8e0f6e6b54d94a6f9317a1c606a3
Leena <lj3827h@gre.ac.uk>
2020-04-02 18:15:13 +0100

    HDE_NPC

--- Menu Prototype/Assets/Scripts/HotDogEating/HDEGameMngr.cs
+++ Menu Prototype/Assets/Scripts/HotDogEating/HDEGameMngr.cs
@@ -10,6 +10,8 @@ public class HDEGameMngr : MonoBehaviour
     public GameObject Opp1;
     public GameObject Opp2;
     public GameObject Opp3;
+    public GameObject OppScores;
+    public GameObject OppScores0;
     Animator Animations;
 
     void Start()
@@ -21,6 +23,8 @@ public class HDEGameMngr : MonoBehaviour
     {
         Instructions.SetActive(true);
         CountDownAnimation.SetActive(false);
+        OppScores.SetActive(false);
+        OppScores0.SetActive(true);
         player.GetComponent<GainPoints>().enabled = false;
         player.GetComponent<EatingFlow>().enabled = false;
         player.GetComponent<EatingTest>().enabled = false;
@@ -47,5 +51,7 @@ public class HDEGameMngr : MonoBehaviour
         Opp1.GetComponent<Animator>().SetBool("Eat", true);
         Opp2.GetComponent<Animator>().SetBool("Eat", true);
         Opp3.GetComponent<Animator>().SetBool("Eat", true);
+        OppScores.SetActive(true);
+        OppScores0.SetActive(false);
     }
 }
